11a2b9233d9c60abb893cd029fa71c5589e77c3d
DC_detective <superaziz1234@gmail.com>
2024-07-12 19:20:42 +0530

    Division is added into master

--- cal_fuc.py
+++ cal_fuc.py
@@ -5,3 +5,9 @@ def addition(a: int,b: int):
 
 def subtraction(a: int , b: int):
     return a-b
+
+def  division(a,b):
+    try:
+        return a/b
+    except:
+        return "Cannot Divide by zero"
--- cal_main.py
+++ cal_main.py
@@ -1,10 +1,13 @@
 from cal_fuc import addition,subtraction
+from cal_fuc import division
+
 def main():
     print("Welcome")
     print("""
     Select which operation do you want to do:
     1. Addition
     2. Subtraction
+    3.division
     """)
 
     option = input("Enter your desired option : ")
@@ -16,6 +19,8 @@ def main():
         return print(addition(a,b))
     elif option == "2":
         return print(subtraction(a,b))
+    elif option == "3":
+        return print(division(a,b))
     else:
         print("Option is invalid")
 
